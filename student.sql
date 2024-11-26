3320b6a3adac75024c879a1e177830260e5a5cf7
UongNgocThanh <thanhun.91@gmail.com>
2024-11-27 00:23:05 +0700

    db

--- student.sql
+++ student.sql
@@ -1,1 +1,12 @@
-drop table customer
+CREATE TABLE students (
+    student_id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Mã SV, sử dụng tự động tăng cho ID
+    age INTEGER,                                   -- Tuổi
+    gender TEXT,                                   -- Giới tính
+    economic_status TEXT,                          -- Tình hình kinh tế
+    gpa REAL,                                      -- GPA
+    credits_completed INTEGER,                     -- Số tín chỉ hoàn thành
+    days_absent INTEGER,                           -- Số ngày vắng (Không phép)
+    part_time_job TEXT,                            -- Làm thêm (TEXT để lưu thông tin về công việc)
+    health_status TEXT,                            -- Tình hình sức khỏe
+    result TEXT                                     -- Kết quả
+);
